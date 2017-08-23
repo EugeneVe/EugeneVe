8db89bf5491e8b059a896c2ce31c55c1c565dc5a
EugeneVe <eugene.veprytskyi@gmail.com>
2017-08-23 10:50:47 +0300

    New view

--- src/app/components/portfolio/portfolio.directive.js
+++ src/app/components/portfolio/portfolio.directive.js
@@ -1,18 +1,19 @@
 export function PortfolioDirective() {
+  'ngInject';
+
+  let directive = {
+    restrict: 'E',
+    templateUrl: 'app/components/portfolio/portfolio.html',
+    controller: MainController,
+    controllerAs: 'vm',
+    bindToController: true
+  };
+  return directive;
+}
+
+class MainController {
+  constructor() {
     'ngInject';
 
-    let directive = {
-        restrict: 'E',
-        templateUrl: 'app/components/portfolio/portfolio.html',
-        controller: MainController,
-        controllerAs: 'vm',
-        bindToController: true
-    };
-
-    return directive;
-}
-class MainController {
-    constructor() {
-        'ngInject';
-    }
+  }
 }
